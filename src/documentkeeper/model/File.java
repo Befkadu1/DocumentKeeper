258ba0ec1145ecf0901600b0d4db30aac4085655
Christoffer Flystam <christoffer.flystam@gmail.com>
2018-01-31 22:24:22 +0100

    chore: finish file class

--- src/documentkeeper/model/File.java
+++ src/documentkeeper/model/File.java
@@ -1,14 +1,57 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package documentkeeper.model;
 
-/**
- *
- * @author christoffer
- */
 public class File {
+    private int id;
+    private int doc_id;
+    private String url;
+    private String name;
+    private String text;
+
+    public File(int id, int doc_id, String url, String name, String text) {
+        this.id = id;
+        this.doc_id = doc_id;
+        this.url = url;
+        this.name = name;
+        this.text = text;
+    }
     
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getDoc_id() {
+        return doc_id;
+    }
+
+    public void setDoc_id(int doc_id) {
+        this.doc_id = doc_id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
 }
