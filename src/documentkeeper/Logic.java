31fd16308b4d446328675ced753329e3da226ae8
Hampus Karlsson <skateor_diie@hotmail.com>
2018-02-01 13:05:54 +0100

    Fixed merge conflict

---
+++ src/documentkeeper/Logic.java
@@ -0,0 +1,59 @@
+package documentkeeper;
+
+import documentkeeper.model.File;
+import documentkeeper.model.Folder;
+import java.util.ArrayList;
+import repository.DBConnection;
+
+/**
+ * @author wuzoink
+ */
+
+public class Logic {
+    
+    private static Logic instance = null;
+    private ArrayList<Folder> folderList = new ArrayList<>();
+    private DBConnection dbConnection = new DBConnection();
+    
+    
+       private Logic() {
+      // Exists only to defeat instantiation.
+   }
+    
+    public static Logic getInstance(){
+        if(instance == null){
+            instance = new Logic();
+        } 
+        return instance;
+    }
+    
+    public ArrayList<Folder> getFolderList(){
+        return folderList;
+    }
+    
+    public Folder getFolderById(int folderId){
+        Folder selectedFolder = new Folder();
+        
+        for(Folder f : folderList){
+            if(f.getId() == folderId){
+                selectedFolder = f;
+            }  
+        }
+        return selectedFolder;
+    }
+    
+    public File getFileById(int fileId){
+        File selectedFile = new File();
+        
+        for(Folder folder : folderList){
+            ArrayList<File> fileList = folder.getFileList();
+            for(File file : fileList){
+                if(file.getId() == fileId){
+                    selectedFile = file;
+                }
+            }
+        }
+        return selectedFile;  
+    }
+    
+}
