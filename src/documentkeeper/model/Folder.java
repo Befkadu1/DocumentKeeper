9889ca61c8ccd86ca633f003c3366d2208a507e9
Andreas Vettefors <contact@vettefors.se>
2018-02-01 09:31:49 +0100

    Created constructors and getter and setters

--- src/documentkeeper/model/Folder.java
+++ src/documentkeeper/model/Folder.java
@@ -1,14 +1,62 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package documentkeeper.model;
 
-/**
- *
- * @author Andreas Vettefors (contact@vettefors.se)
- */
+import java.util.ArrayList;
+
 public class Folder {
-    
+    private int id;
+    private String name;
+    private String description;
+    private int category_id;
+    private ArrayList<File> fileList;
+
+    public Folder() {
+    }
+
+    public Folder(int id, String name, String description, int category_id, ArrayList<File> fileList) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.category_id = category_id;
+        this.fileList = fileList;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getCategory_id() {
+        return category_id;
+    }
+
+    public void setCategory_id(int category_id) {
+        this.category_id = category_id;
+    }
+
+    public ArrayList<File> getFileList() {
+        return fileList;
+    }
+
+    public void setFileList(ArrayList<File> fileList) {
+        this.fileList = fileList;
+    }  
 }
